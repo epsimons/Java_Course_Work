14f362f767fb2c421a9e3d513d9cdf18105ea39f
Ethan <ethanpsimons38@gmail.com>
2017-03-17 14:30:25 -0400

    Update to ReadXML
    I combined this with swing and HTML/CSS to create a formatted table.

--- WindowTest.java
+++ WindowTest.java
@@ -3,36 +3,82 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package windowtest;
+package readxml;
 
-
-import java.awt.*;
-import javax.swing.*;
 /**
  *
  * @author Ethan
  */
-public class WindowTest {
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class ReadXML {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
+            try {
+
+	File fXmlFile = new File("/Users/Ethan/NetBeansProjects/ReadXML/src/readxml/employee.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+	doc.getDocumentElement().normalize();
+	NodeList nodeList = doc.getElementsByTagName("employee");
         
-       String winTitle = "Java Window Demo";
-       String message = "I am made with Java!";
-       createWindow(winTitle, message);
-     
+        String winTitle = "Employees";
+        String message = "<html><head><style>body{padding:10px;}th{background-color: blue;color:yellow;}</style></head><body><h1>Acme Portable Hole Company</h1><table border='1' style='border-collapse:collapse;'><tr><th>Employee id</th><th>First Name</th><th>Last Name</th><th>Position</th><th>Salary</th></tr>";
+        
+	//System.out.println("Acme Portable Hole Company");
+
+	for (int temp = 0; temp < nodeList.getLength(); temp++) {
+
+		Node nNode = nodeList.item(temp);
+
+		//System.out.println();
+                
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element eElement = (Element) nNode;
+                        /*
+			System.out.println("Employee id : " + eElement.getAttribute("id"));
+			System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+			System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+			System.out.println("Position : " + eElement.getElementsByTagName("position").item(0).getTextContent());
+			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+                        */
+                        message += "<tr><td>" + eElement.getAttribute("id") + "</td><td>" + eElement.getElementsByTagName("firstname").item(0).getTextContent() + "</td><td>" + eElement.getElementsByTagName("lastname").item(0).getTextContent() + "</td><td>" + eElement.getElementsByTagName("position").item(0).getTextContent() + "</td><td>" + eElement.getElementsByTagName("salary").item(0).getTextContent()+"</td></tr>";
+                        
+		}     
+	}
+                        message += "</table></body><html>";
+                        createWindow(winTitle, message);
+    } catch (Exception e) {
+	e.printStackTrace();
+}
+            //System.exit(0);
     }
-    public static void createWindow(String name, String msg)
+    
+
+public static void createWindow(String name, String msg)
     {
         JFrame frame = new JFrame(name);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel textLabel = new JLabel(msg,SwingConstants.CENTER); textLabel.setPreferredSize(new Dimension(300, 200)); 
+        JLabel textLabel = new JLabel(msg,SwingConstants.CENTER); textLabel.setPreferredSize(new Dimension(450, 300)); 
         frame.getContentPane().add(textLabel, BorderLayout.CENTER); 
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
-    }
-    
+}
+
 }
